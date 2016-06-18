fc90e3f9188b0386509b062c71aefb5989a005ff
shankha-phoenix <shankhadeep.ghosh@gmail.com>
2016-06-18 21:45:00 +0530

    Widening primitive

---
+++ WideningPrimitive/src/org/practice/WideningPrimitive.java
@@ -0,0 +1,17 @@
+package org.practice;
+
+/**
+ * Program : WideningPrimitive.java
+ * Author : Shankhadeep Ghosh
+ * Email : shankhadeep.ghosh@gmail.com
+ */
+
+public class WideningPrimitive {
+
+	public static void main(String[] args) {
+		System.out.print("Y" + "O");
+        System.out.print('L'+ 'O');
+
+	}
+
+}
