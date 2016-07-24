d61cae375c4f1d1928888a7414e010e084777d26
shankha-phoenix <shankhadeep.ghosh@gmail.com>
2016-07-24 23:56:06 +0530

    Bubble Sort Implementation

---
+++ BubbleSort/src/org/practice/BubbleSort.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package org.practice;
+
+/**
+ * Program : Bubble Sort
+ * Author : Shankhadeep Ghosh
+ * Email : shankhadeep.ghosh@gmail.com
+ */
+public class BubbleSort {
+	
+	public void bubbleSort(int[] myArray) {
+		
+		for(int i=0; i<myArray.length; i++) {
+			for(int j=i+1; j<myArray.length; j++) {
+				if(myArray[i] > myArray[j]) {
+					int temp = myArray[i];
+					myArray[i] = myArray[j];
+					myArray[j] = temp;
+				}
+			}
+		}
+		
+		System.out.println("Sorted Array ");
+		for (int i : myArray) {
+			System.out.print(i);
+		}
+	}
+	
+}
---
+++ BubbleSort/src/org/practice/BubbleSortDemo.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package org.practice;
+
+/**
+ * Program : 
+ * Author : Shankhadeep Ghosh
+ * Email : shankhadeep.ghosh@gmail.com
+ */
+public class BubbleSortDemo {
+
+	public static void main(String[] args) {
+		
+		BubbleSort bs = new BubbleSort();
+		int[] myArray = {1,2,9,5,3,4,8,56,4,32,156};
+		
+		bs.bubbleSort(myArray);
+
+	}
+
+}
