c4d4857cd1b76a5af3a11ac890568111deb534af
Shankhadeep <shankhadeep.ghosh@gmail.com>
2017-11-24 08:08:13 +0530

    SpringBoot Applications
    Understanding SpringBoot

---
+++ SpringBoot/FirstApp/src/main/java/boot/intro/Application.java
@@ -0,0 +1,31 @@
+package boot.intro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            System.out.println("Lets inspect the beans of Spring Boot.. ");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+
+        };
+    }
+}
---
+++ SpringBoot/FirstWebApp/src/main/java/boot/security/Application.java
@@ -0,0 +1,12 @@
+package boot.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) throws Throwable{
+        SpringApplication.run(Application.class, args);
+    }
+}
